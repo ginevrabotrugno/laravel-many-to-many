0f5f048cddee6f98b171a836245fa8e42e8080ef
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-25 16:25:35 +0200

    feat: add technologies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            TechnologiesTableSeeder::class,
             TypeTableSeeder::class,
             ProjectSeeder::class
         ]);
---
+++ database/seeders/TechnologiesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Technology;
+use App\Functions\Helper;
+
+class TechnologiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = ['Front End', 'Back End', 'Design', 'UX', 'Laravel', 'VueJs', 'Angular', 'React'];
+
+        foreach($data as $technology){
+            $new_technology = new Technology();
+            $new_technology->name = $technology;
+            $new_technology->slug = Helper::generateSlug($new_technology->name, Technology::class);
+            $new_technology->save();
+        }
+    }
+}
